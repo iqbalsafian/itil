78e158b7e4ea09692c0c8e29f70ebc875a9e84e4
Mohd Iqbal <iqbal.safian@gmail.com>
2017-08-23 09:19:02 +0800

    Create login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import LoginPage from './components/LoginPage'
+import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <LoginPage />
       </div>
     );
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Card } from 'antd';
+const FormItem = Form.Item;
 
 class LoginPage extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
   render() {
     return(
-      <div>
-        
+      <div style={{position: 'absolute', margin: '0', top: '50%', right: '50%', transform: 'translate(50%,-50%)'}}>
+        <Card style={{ width: '350px' }}>
+          <Form onSubmit={this.handleSubmit}>
+            <FormItem>
+              <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Email" />
+            </FormItem>
+            <FormItem>
+              <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+            </FormItem>
+            <FormItem>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+            </FormItem>
+          </Form>
+        </Card>
       </div>
     )
   }
