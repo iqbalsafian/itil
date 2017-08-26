0d6147cae930eab45cc651153a0df058ed473aa3
Mohd Iqbal <iqbal.safian@gmail.com>
2017-08-26 23:14:06 +0800

    Modify username and password for the database

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,8 +6,8 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'itil',
-      user:     'iqbal',
-      password: 'iqbal007'
+      user:     'user',
+      password: 'password'
     },
     pool: {
       min: 2,
