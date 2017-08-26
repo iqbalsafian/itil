728036f30d5a8976d10ae7f0d076adf9e44febc3
Mohd Iqbal <iqbal.safian@gmail.com>
2017-08-27 05:23:37 +0800

    Fix knexfile configuration

---
+++ server/knexfile.js
@@ -0,0 +1,56 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'itil',
+      user:     'theuser',
+      password: 'thepassword'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'itil',
+      user:     'iqbal',
+      password: 'iqbal007'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
